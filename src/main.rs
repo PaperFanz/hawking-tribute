31115456227a0556122f80de046a708f6e38021a
PaperFanz <petefan.7@gmail.com>
2018-03-14 00:15:15 -0500

    correct typos and add formatting

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
     let quotes = [ "Intelligence is the ability to adapt to change.", 
                     "The greatest enemy of knowledge is not ignorance, it is the illusion of knowledge.", 
                     "I have noticed even people who claim everything is predestined, and that we can do nothing to change it, look before they cross the road.", 
-                    "We are just an advanced breed of monkeys of a minor planet of a very average star. But we can understand the Univers. That makes us something very special.", 
+                    "We are just an advanced breed of monkeys of a minor planet of a very average star. But we can understand the Universe. That makes us something very special.", 
                     "My goal is simple. It is a complete understanding of the universe, why it is as it is and why it exists at all.", 
                     "Life would be tragic if it weren't funny.", 
                     "I have no idea. People who boast about their IQ are losers.",
@@ -17,6 +17,6 @@ fn main() {
 
     let q_num = rand::thread_rng().gen_range(1,quotes.len());
 
-    println!("{quote} -Stephen Hawking, 1942-2018", quote = quotes[q_num]);
+    println!("\"{quote}\" \n-Stephen Hawking, 1942-2018", quote = quotes[q_num]);
 
 }
